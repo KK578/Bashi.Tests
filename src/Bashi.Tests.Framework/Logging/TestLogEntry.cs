a27eeabe1c8a75d59526f093161d2bcb32be48f6
KK578 <KevinKwan95@googlemail.com>
2020-04-13 22:54:31 +0100

    BASHI-37: Implement TestableLogger and tests | Add TestableLogger

---
+++ src/Bashi.Tests.Framework/Logging/TestLogEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace Bashi.Tests.Framework.Logging
+{
+    public class TestLogEntry
+    {
+        public TestLogEntry(LogLevel logLevel, Exception exception, string message)
+        {
+            this.LogLevel = logLevel;
+            this.Exception = exception;
+            this.Message = message;
+        }
+
+        public LogLevel LogLevel { get; }
+        public Exception Exception { get; }
+        public string Message { get; }
+    }
+}
